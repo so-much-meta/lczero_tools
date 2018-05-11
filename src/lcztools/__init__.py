3e1e021b2713442d7ae4ce8bd52e28619f808331
so-much-meta <38630665+so-much-meta@users.noreply.github.com>
2018-05-11 01:01:34 -0400

    Remove changes to python path

--- src/lcztools/__init__.py
+++ src/lcztools/__init__.py
@@ -3,8 +3,4 @@
 from lcztools._idx_to_move import idx_to_move
 from lcztools._leela_board import LeelaBoard
 from lcztools._leela_net import load_network, LeelaNet
-import os
-import sys
 
-
-sys.path.append(os.path.expanduser('~/git/leela-chess/training/tf'))
