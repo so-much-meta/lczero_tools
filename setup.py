ee1b9dc6bc9cb39df94ad820d163e059e7af1ab8
Eric Langlois <eric@langlois.xyz>
2018-05-15 13:38:19 -0400

    Added basic setup.py to enable installing as a python package.

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Package setup script."""
+import setuptools
+
+setuptools.setup(
+    name='lczero-tools',
+    version='0.1',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'python-chess',
+    ],
+    extras_require={
+        'tf': ['tensorflow'],
+        'tf-gpu': ['tensorflow-gpu'],
+        'torch': ['torch'],
+    },
+    setup_requires=[],
+    tests_require=[],
+)
