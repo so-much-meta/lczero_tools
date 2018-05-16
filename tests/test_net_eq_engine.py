2a49307bc0189796e637be3d400b50cbd03ee9b3
so-much-meta <>
2018-05-15 20:20:34 -0400

    Show time information

--- tests/test_net_eq_engine.py
+++ tests/test_net_eq_engine.py
@@ -1,6 +1,8 @@
 '''
 This script just ensures that the engine is equal to the python implementation in evaluation
 If no exception is thrown, it has passed
+
+
 '''
 
 import os
@@ -10,6 +12,7 @@ import lcztools
 from lcztools.testing.leela_engine import LCZEngine
 import numpy as np
 import chess.pgn
+import time
 
 
 engine_path = os.path.expanduser('~/git/leela-chess/release/lczero')
@@ -20,9 +23,9 @@ engine.evaluate(board.lcz_to_uci_engine_board())
 net = lcztools.load_network('pytorch', weights_file)
 
 
-def eval_equal(net, engine, lczboard, tolerance=.00006):
-    npol, nv = net.evaluate(lczboard)
-    epol, ev = engine.evaluate(lczboard.lcz_to_uci_engine_board())
+def eval_equal(neteval, engineeval, tolerance=.00006):
+    npol, nv = neteval
+    epol, ev = engineeval
     for uci in npol:
         if abs(npol[uci] - epol[uci]) > tolerance:
             return False
@@ -30,6 +33,9 @@ def eval_equal(net, engine, lczboard, tolerance=.00006):
         return False
     return True
 
+net_eval_time = 0
+engine_eval_time = 0
+totalevals = 0
 numgames = 200
 for gamenum in range(numgames):
     print("Playing game {}/{}".format(gamenum+1,numgames))
@@ -39,22 +45,31 @@ for gamenum in range(numgames):
         counter -= 1
         if board.can_claim_draw():
             counter = max(counter, 10)
-        if not eval_equal(net, engine, board):
-            raise Exception("Not equal:", ' '.join(m.uci() for m in board.move_stack))
+        
+        clock = time.time()
         policy, value = net.evaluate(board)
+        net_eval_time += time.time() - clock
+        
+        engine_board = board.lcz_to_uci_engine_board()
+        clock = time.time()
+        epolicy, evalue = engine.evaluate(engine_board)
+        engine_eval_time += time.time() - clock
+        
+        totalevals += 1
+        
+        if not eval_equal((policy, value), (epolicy, evalue)):
+            raise Exception("Not equal:", ' '.join(m.uci() for m in board.move_stack))
         ucis = list(policy)
         pol_values = np.fromiter(policy.values(), dtype=np.float32)
         pol_values = pol_values/pol_values.sum()
         pol_index = np.random.choice(len(pol_values), p=pol_values)
         uci = ucis[pol_index]
-        #print()
-        #print(board)
-        #print(policy)
-        #print(value)
         board.push_uci(uci)
     print()
     game = chess.pgn.Game.from_board(board.lcz_to_board())
     print(game)
     print(board)
+    print("Average net eval time   : {:.6f}".format(net_eval_time/totalevals))
+    print("Average engine eval time: {:.6f}".format(engine_eval_time/totalevals))
     print("All Evals Equal: PASS")
 print("Network looks good!")
